c4691be33bc28473d094111f24554052cab70752
xandowski <xandy-moraes@hotmail.com>
2022-03-31 23:49:33 -0300

    create UserSchema

--- crud_flask/serializer.py
+++ crud_flask/serializer.py
@@ -1,6 +1,6 @@
-from flask_marshmallow import Marshmallow
+from flask_marshmallow import Marshmallow, fields
 
-from .model import Task
+from .model import Task, User
 
 ma = Marshmallow()
 
@@ -12,3 +12,10 @@ def configure(app):
 class TaskSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Task
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+    username = fields.Str(required=True)
