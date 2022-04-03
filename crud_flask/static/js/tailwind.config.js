35d1acc5ef2914a2b4211da5f293f0cf351687cb
xandowski <xandy-moraes@hotmail.com>
2022-04-03 18:04:06 -0300

    feature: responsive home page

---
+++ crud_flask/static/js/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#208AAE',
+                'primary-dark': '#0D2149',
+                'secondary': '#F9EBE0'
+            }
+        }
+    }
+}
